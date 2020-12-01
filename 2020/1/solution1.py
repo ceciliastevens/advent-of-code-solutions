14edb7a96d1b88e933af977572ab198b242ca64a
Cecilia Stevens <63068179+ceciliastevens@users.noreply.github.com>
2020-12-01 07:31:23 -0600

    initial commit

---
+++ 2020/1/solution1.py
@@ -0,0 +1,9 @@
+with open("input.txt", "r") as file:
+	numbers = file.readlines()
+
+for number in numbers:
+	for multiplier in numbers:
+		if(int(number) + int(multiplier) == 2020)
+			solution = int(number) * int(multiplier)
+
+print(solution)
---
+++ 2020/1/solution2.py
@@ -0,0 +1,10 @@
+with open("input.txt", "r") as file:
+	numbers = file.readlines()
+
+for number1 in numbers:
+	for number2 in numbers:
+		for number3 in numbers:
+			if(int(number1) + int(number2) + int(number3) == 2020):
+				solution = int(number1) * int(number2) * int(number3)
+
+print(solution)
