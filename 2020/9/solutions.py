36e27ccc02ea95d549a80c89ee32e1425a80d384
Cecilia Stevens <63068179+ceciliastevens@users.noreply.github.com>
2020-12-09 08:30:18 -0600

    added day 9

---
+++ 2020/9/solutions.py
@@ -0,0 +1,32 @@
+from itertools import combinations
+
+def get_input(filename):
+	return [int(line.strip()) for line in open(filename, "r")]
+
+def find_first_failure(sequence, preamble_length):
+	for index in range(preamble_length, len(sequence)):
+		preamble = set(sequence[index-preamble_length:index])
+		if sequence[index] not in [sum(combination) for combination in combinations(preamble, 2)]:
+			return sequence[index], index
+
+def find_sum_range(sequence, target):
+	for starting_point in range(len(sequence)):
+		contiguous_set = set()
+		current_index = starting_point
+		while sum(contiguous_set) < target:
+			contiguous_set.add(sequence[current_index])
+			current_index += 1
+		if sum(contiguous_set) == target and len(contiguous_set) > 1:
+			return min(contiguous_set) + max(contiguous_set)
+
+
+if __name__ == "__main__":
+	preamble_length = 25
+	sequence = get_input("input.txt")
+
+	# part 1 solution
+	failure, index = find_first_failure(sequence, preamble_length)
+	print("The first number that is not a sum of two of the {} previous numbers is {}".format(preamble_length, failure))
+
+	#part 2 solution
+	print("The sum of the minimum and maximum of the contiguous set of two or more numbers summing up to {} is {}".format(failure, find_sum_range(sequence, failure)))
