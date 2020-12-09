a5a29e6d7f62a348df86cdabacf7377826272ef9
Cecilia Stevens <63068179+ceciliastevens@users.noreply.github.com>
2020-12-09 08:31:45 -0600

    small cleanup

--- 2020/9/solutions.py
+++ 2020/9/solutions.py
@@ -7,7 +7,7 @@ def find_first_failure(sequence, preamble_length):
 	for index in range(preamble_length, len(sequence)):
 		preamble = set(sequence[index-preamble_length:index])
 		if sequence[index] not in [sum(combination) for combination in combinations(preamble, 2)]:
-			return sequence[index], index
+			return sequence[index]
 
 def find_sum_range(sequence, target):
 	for starting_point in range(len(sequence)):
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 	sequence = get_input("input.txt")
 
 	# part 1 solution
-	failure, index = find_first_failure(sequence, preamble_length)
+	failure = find_first_failure(sequence, preamble_length)
 	print("The first number that is not a sum of two of the {} previous numbers is {}".format(preamble_length, failure))
 
 	#part 2 solution
