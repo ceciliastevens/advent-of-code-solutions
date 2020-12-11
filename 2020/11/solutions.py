712fde826d996a971cbd0104b0f360aa47fc1f3c
Cecilia Stevens <63068179+ceciliastevens@users.noreply.github.com>
2020-12-11 11:35:02 -0600

    added solution to puzzle 2

--- 2020/11/solutions.py
+++ 2020/11/solutions.py
@@ -26,20 +26,21 @@ def get_adjacent_seats(seat_map, x_pos, y_pos):
 	return adjacent_occupied_seat_count
 
 
-def get_next_seat_state(seat_map: list, x_pos: int, y_pos: int): #assumes it is actually a seat
-	if is_seat_available(seat_map, x_pos, y_pos) and get_adjacent_seats(seat_map, x_pos, y_pos) == 0:
+def get_next_seat_state(seat_map: list, x_pos: int, y_pos: int, seat_validator = get_adjacent_seats, seats_threshhold = 4): #assumes it is actually a seat
+	seats_seen = seat_validator(seat_map, x_pos, y_pos)
+	if is_seat_available(seat_map, x_pos, y_pos) and seats_seen == 0:
 		return '#'
-	elif seat_map[y_pos][x_pos] == '#' and get_adjacent_seats(seat_map, x_pos, y_pos) >= 4:
+	elif seat_map[y_pos][x_pos] == '#' and seats_seen >= seats_threshhold:
 		return 'L'
 	else:
 		return seat_map[y_pos][x_pos]
 
-def get_state(seat_map):
+def get_state(seat_map, seat_validator = get_adjacent_seats, seats_threshhold = 4):
 	next_seat_map = deepcopy(seat_map)
 	for y_pos in range(len(seat_map)):
 		for x_pos in range(len(seat_map[y_pos])):
 			if seat_map[y_pos][x_pos] != '.':
-				next_seat_map[y_pos][x_pos] = get_next_seat_state(seat_map, x_pos, y_pos)
+				next_seat_map[y_pos][x_pos] = get_next_seat_state(seat_map, x_pos, y_pos, seat_validator, seats_threshhold)
 	return next_seat_map
 
 def count_occupied_seats(seat_map):
@@ -56,15 +57,45 @@ def print_seat_map(seat_map):
 		for item in row:
 			print(item, end="")
 	print("")
-		
 
+OPERATIONS = [
+	lambda x: x + 1,
+	lambda x: x - 1,
+	lambda x: x
+]	
+
+def get_visible_seats(seat_map, x_pos, y_pos):
+	visible_seats = 0
+	for y_operation in OPERATIONS:
+		for x_operation in OPERATIONS:
+			current_x_pos = x_pos
+			current_y_pos = y_pos
+			while 0 <= current_x_pos < len(seat_map[0]) and 0 <= current_y_pos < len(seat_map):
+				current_x_pos = x_operation(current_x_pos)
+				current_y_pos = y_operation(current_y_pos)
+				if current_x_pos == x_pos and current_y_pos == y_pos:
+					break
+				if is_seat(seat_map, current_x_pos, current_y_pos):
+					if is_seat_occupied(seat_map, current_x_pos, current_y_pos):
+						visible_seats+=1
+					break
+	return visible_seats
 
 if __name__ == '__main__':
-	seat_map = get_input("input.txt")
+	original_seat_map = get_input("input.txt")
 
 	#puzzle 1 solution
 	previous_seat_map = []
+	seat_map = deepcopy(original_seat_map)
 	while seat_map != previous_seat_map:
 		previous_seat_map = deepcopy(seat_map)
 		seat_map = get_state(seat_map)
 	print("At first, there are {} occupied seats.".format(count_occupied_seats(seat_map)))
+
+	#puzzle 2 solutions
+	previous_seat_map = []
+	seat_map = deepcopy(original_seat_map)
+	while seat_map != previous_seat_map:
+		previous_seat_map = deepcopy(seat_map)
+		seat_map = get_state(seat_map, get_visible_seats, 5)
+	print("Using updated rules, there are {} occupied seats.".format(count_occupied_seats(seat_map)))
